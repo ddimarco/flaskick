6dde34f2f8437b5720608faad2c76c3ed3f0b1f6
Daniel Di Marco <d.dimarco@gmx.de>
2017-10-05 17:10:20 +0200

    remove typo & fixme

--- flaskick/static/update.js
+++ flaskick/static/update.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
     $("#refresh-btn").click(function() {
         $("#refresh-status").text("refreshing backend...");
-            console.log("refreshing");
-        // FIXME
         $.get('/_db_refresh', function(data,status) {
             $.get('/_db_state', function(data) {
                 $("#refresh-status").text('last update: ' + data.state);
